a3d9c7917417ce7a0c7c06fe2fa2d1ec832e030d
Oluwajoba <oluwajobabello@gmail.com>
2019-10-11 02:49:52 +0100

    wire up and connect counter component with redux-store / counter / redux-store / project set-up

--- async-redux/src/App.js
+++ async-redux/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { combineReducers, createStore } from "redux";
+import {Provider} from "react-redux"
+
 import './App.css';
+import Counter from './components/Counter';
+import * as reducers from './states/reducers';
 
 function App() {
+
+  const monsterReducer = combineReducers({
+    count: reducers.counterReducer,
+  });
+
+  const store = createStore(
+    monsterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
+
   return (
+    <Provider store={store}>
     <div className="App">
       <h1>Async Redux - React Redux App</h1>
+      <Counter />
     </div>
+    </Provider>
   );
 }
 
